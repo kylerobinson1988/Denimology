7cd74571702ac2f6fc35a9401b60b7bb24e90833
Sinkingstarship <kylerobinson1988@gmail.com>
2019-01-07 08:50:20 -0500

    Core Data
    Fetching and retrieving denim items from Core Data - Tests working.

--- Models/DenimItem.swift
+++ Models/DenimItem.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 enum DenimItemType: String {
     
@@ -54,6 +55,94 @@ class DenimItem {
     var measurements: ClothingMeasurement?
     
     var journal: [EntryLog] = []
+ 
+    func saveToCoreData() {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "DenimItemCore", in: managedContext) else { return }
+        
+        let denimItem = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        denimItem.setValue(brand, forKey: "brand")
+        denimItem.setValue(name, forKey: "name")
+        denimItem.setValue(type, forKey: "type")
+        denimItem.setValue(size, forKey: "size")
+        denimItem.setValue(sanforized, forKey: "sanforized")
+        denimItem.setValue(weight, forKey: "weight")
+        denimItem.setValue(dateStarted, forKey: "dateStarted")
+        denimItem.setValue(hoursWorn, forKey: "hoursWorn")
+        denimItem.setValue(daysWorn, forKey: "daysWorn")
+        denimItem.setValue(washes, forKey: "washes")
+        // Measurements
+        // Journal
+        
+        do {
+            
+            try managedContext.save()
+            
+        } catch let error as NSError {
+            
+            print("Could not save. \(error), \(error.userInfo)")
+            
+        }
+        
+    }
+    
+    static func createFromCoreData(coreObject: NSManagedObject) -> DenimItem {
+        
+        let newDenimItem = DenimItem()
+        
+        guard let denimItemCoreObject = coreObject as? DenimItemCore else { return newDenimItem }
+        
+        newDenimItem.brand = denimItemCoreObject.brand
+        newDenimItem.name = denimItemCoreObject.name
+        newDenimItem.type = DenimItemType(rawValue: denimItemCoreObject.type ?? "")
+        newDenimItem.size = Int(denimItemCoreObject.size)
+        newDenimItem.sanforized = denimItemCoreObject.sanforized
+        newDenimItem.weight = denimItemCoreObject.weight
+        newDenimItem.dateStarted = denimItemCoreObject.dateStarted
+        newDenimItem.daysWorn = Int(denimItemCoreObject.daysWorn)
+        newDenimItem.washes = Int(denimItemCoreObject.washes)
+        
+        // Journal
+        // Measurements
+        
+        return newDenimItem
+        
+    }
+    
+    static func fetchDenimItems() -> [DenimItem] {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return [] }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "DenimItemCore")
+        
+        var denimCoreObjects: [NSManagedObject] = []
+        
+        do {
+            
+            denimCoreObjects = try managedContext.fetch(fetchRequest)
+            
+        } catch let error as NSError {
+            
+            print("Could not save. \(error), \(error.userInfo)")
+            
+        }
+        
+        let denimItems = denimCoreObjects.map {
+            
+            DenimItem.createFromCoreData(coreObject: $0)
+            
+        }
+        
+        return denimItems
+        
+    }
     
 }
 
